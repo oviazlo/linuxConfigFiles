65fc756a7da5285953989bcb9dd0251c7c1add5e
Oleksandr Viazlo (X1 Carbon) <oleksandr.viazlo@cern.ch>
2018-11-27 15:13:33 +0100

    update install-file

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "No argument supplied! Choose your setup!"
+	echo "[Arch, SL6]"
+	exit
+else
+	LOCAL_FILE="$1"
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 for file in $(find $DIR -name '*.tocopy'); do
